e35530fccc5310b7c1931333b68852f7682e5280
Chirayu <chirayupancholi17@gmail.com>
2023-11-11 17:20:12 +0530

    feat(db): dbconfig - connection to db

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv';
+import connectDB from './utils/dbConfig';
 
 const app = express();
 const port = 3000;
+
+dotenv.config()
+
+connectDB()
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World")
